a91758879233cfbdd3ba093b77a4c67220b815d8
Christian <christian@KieransLaptop.local>
2017-08-26 17:07:16 +1200

    Fix https fetch

--- src/utils/locationsender.js
+++ src/utils/locationsender.js
@@ -2,7 +2,7 @@ import geolocator from './geolocator';
 
 function getListings(callback) {
 	geolocator(function(params) {
-		fetch('http://quiky.herokuapp.com/listings?lat=' + params.lat + '&long=' + params.long)
+		fetch('https://quiky.herokuapp.com/listings?lat=' + params.lat + '&long=' + params.long)
 		.then(function(response) {
 			return response.json();
 		}).then(function(json) {
