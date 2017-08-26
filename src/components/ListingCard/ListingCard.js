663c0d7fcc35ee1622ad51cb0f1a81d5cb771558
Christian <christian@KieransLaptop.local>
2017-08-27 09:51:41 +1200

    Make listings clickable

--- src/components/ListingCard/ListingCard.js
+++ src/components/ListingCard/ListingCard.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import LazyImage from '../LazyImage/LazyImage';
 import './ListingCard.css';
 
 const ListingCard = ({ listing: { distance, image, title, price } }) =>
   <div className="ListingCard__container">
+    <Link 
+      className="ListingCard__overlay" 
+      to="/listing"
+    />
     <LazyImage src={image} styleName={"ListingCard__image"}/>
     <div className="ListingCard__details">
       <div className="ListingCard__top-row">
