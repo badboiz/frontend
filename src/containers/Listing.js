c2226f390c02a7b842259d6bd80e1af766fb3a91
justinvos <justinvosnz@gmail.com>
2017-08-27 14:38:24 +1200

    Added listing paged

--- src/containers/Listing.js
+++ src/containers/Listing.js
@@ -1,6 +1,21 @@
 import React from 'react';
 
-const Listing = ({ match: { params } }) =>
-  <h1>I'm listing {params.listingid}</h1>;
+class Listing extends React.PureComponent {
+  state = { loaded: false };
+
+  render() {
+    return (
+      <div class='listingPage'>
+        <h2>I'm listing {this.props.match.params.listingid}</h2>
+        <img id='listingImage' />
+        <div className="">
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Listing;
+
+
+//this.props..match.params.listingid
