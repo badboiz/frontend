78792e565dd965e315ff1233f921f260e9e9542e
Christian <christian@KieransLaptop.local>
2017-08-27 12:48:25 +1200

    remove getlistings call from index

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import getListings from './utils/locationsender';
 
-getListings(console.log);
 ReactDOM.render(
   <Router>
     <App />
