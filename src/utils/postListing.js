985dfa6943805876aca024330e0b7e205dd97e47
Christian <christian@KieransLaptop.local>
2017-08-27 12:47:16 +1200

    Fix postlisting

--- src/utils/postListing.js
+++ src/utils/postListing.js
@@ -3,8 +3,8 @@ export default function postListing(listing) {
     const xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://quiky.herokuapp.com/listings');
     xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-    xhr.onload = resolve;
-    xhr.onerror = reject;
+    xhr.onload = () => resolve(xhr.responseText);
+    xhr.onerror = () => reject(xhr.statusText);
     xhr.send(JSON.stringify(listing));
   });
 }
